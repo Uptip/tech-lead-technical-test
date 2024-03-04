4ff150a9233fb755d6cfea8c34ecd8dddf017da9
Henri Michel <tip@me.com>
2024-03-04 17:07:02 +0100

    refactor: move main function to its own file

--- app.js
+++ app.js
@@ -1,27 +1,10 @@
 #!/usr/bin/node
 
-const { data } = require('./data');
-const { filterAnimals, countChildren } = require('./lib');
-const { parseArguments } = require('./utils/parseArguments');
+const { main } = require('./lib');
 
-const run = () => {
-  try {
-    const { filter, count } = parseArguments(process.argv);
-    let result = data;
-
-    if (filter) {
-      result = filterAnimals({ data: result, filter });
-    }
-
-    if (count) {
-      result = countChildren(result);
-    }
-
-    return result;
-  } catch (err) {
-    console.error(`Error: \n\n${err.message}`);
-    process.exit(1);
-  }
-};
-
-console.log(JSON.stringify(run(), null, 2));
+try {
+  console.log(JSON.stringify(main(), null, 2));
+} catch (err) {
+  console.error(`Error: ${err.message}`);
+  process.exit(1);
+}
